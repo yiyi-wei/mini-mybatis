9a598d098598bc8fc025cadc2f4550833e6d2534
han.wei <han.wei.work2023@gmail.com>
2024-02-15 23:08:47 +0800

    init project

---
+++ src/main/java/ltd/weiyiyi/App.java
@@ -0,0 +1,13 @@
+package ltd.weiyiyi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
